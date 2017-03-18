27b2367b7cfc6bef15dc46106d45eb4c8e6c9a39
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-03-18 02:25:53 -0300

    hello API

---
+++ src/RestgramApplication.java
@@ -0,0 +1,22 @@
+import com.jgabrielfreitas.HelloWorld;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by JGabrielFreitas on 18/03/17.
+ */
+
+@ApplicationPath("/")
+public class RestgramApplication extends Application {
+
+    //The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet h = new HashSet<Class<?>>();
+        h.add( HelloWorld.class );
+        return h;
+    }
+}
